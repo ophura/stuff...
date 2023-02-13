391f091d17ea2ac6dd3837f6f3c4dc9f3db7aa38
Ophura <61603742+ophura@users.noreply.github.com>
2023-02-13 14:07:05 +0100

    Update AnimationClipModifier.cs

--- AnimationClipModifier.cs
+++ AnimationClipModifier.cs
@@ -1,50 +1,52 @@
-using UnityEditor;
-using UnityEngine;
-
-
-internal class AnimationClipModifier : EditorWindow
+namespace ophura.jp
 {
-    [MenuItem("Tools/AnimationClip Modifier")]
-    private static void FocusOrDisplayWindow() =>
-        _ = GetWindow<AnimationClipModifier>(false, "AnimationClip Modifier", true);
+    using UnityEditor;
+    using UnityEngine;
 
 
-    private void OnGUI()
+    internal class AnimationClipModifier : EditorWindow
     {
-        loopTime = EditorGUILayout.Toggle("Loop Time", loopTime);
+        [MenuItem("Tools/AnimationClip Modifier")]
+        private static void FocusOrDisplayWindow() =>
+            _ = GetWindow<AnimationClipModifier>(false, "AnimationClip Modifier", true);
 
-        if (GUILayout.Button("Set Loop Time"))
+
+        private void OnGUI()
         {
-            SetAnimationClipLoopTime(loopTime);
+            loopTime = EditorGUILayout.Toggle("Loop Time", loopTime);
+
+            if (GUILayout.Button("Set Loop Time"))
+            {
+                SetAnimationClipLoopTime(loopTime);
+            }
         }
-    }
 
 
-    // https://forum.unity.com/threads/how-to-enable-looptime-property-of-animationclip-via-code.832633/
-    private void SetAnimationClipLoopTime(bool value)
-    {
-        string[] clipGUIDs = AssetDatabase.FindAssets(
-            $"t:{nameof(AnimationClip)}", new[] { "Assets" }
-            );
-
-        foreach (string clipGUID in clipGUIDs)
+        private void SetAnimationClipLoopTime(bool value)
         {
-            string clipPath = AssetDatabase.GUIDToAssetPath(clipGUID);
-
-            AnimationClip clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(clipPath);
-
-            AnimationClipSettings clipSettings = AnimationUtility.GetAnimationClipSettings(
-                clip
+            string[] clipGUIDs = AssetDatabase.FindAssets(
+                $"t:{nameof(AnimationClip)}", new[] { "Assets" }
                 );
 
-            clipSettings.loopTime = value;
+            foreach (string clipGUID in clipGUIDs)
+            {
+                string clipPath = AssetDatabase.GUIDToAssetPath(clipGUID);
 
-            AnimationUtility.SetAnimationClipSettings(clip, clipSettings);
+                AnimationClip clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(clipPath);
+
+                AnimationClipSettings clipSettings = AnimationUtility.GetAnimationClipSettings(
+                    clip
+                    );
+
+                clipSettings.loopTime = value;
+
+                AnimationUtility.SetAnimationClipSettings(clip, clipSettings);
+            }
+
+            AssetDatabase.SaveAssets();
         }
 
-        AssetDatabase.SaveAssets();
+
+        private bool loopTime;
     }
-
-
-    private bool loopTime;
 }
